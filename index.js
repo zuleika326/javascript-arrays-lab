9491cbb6d60a319b6adcc55bcd8e16e47e6a11b6
zuleika <njokizuleika@gmail.com>
2023-02-24 13:52:34 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; 
 
-// Add your functions and code here
+
+function destructivelyPrependKitten(name){
+    kittens.unshift(name);
+}
+
+function destructivelyAppendKitten(name){
+kittens.push (name);
+}
+function appendKitten(name){
+  return  kittens.concat(name);
+}
+function destructivelyRemoveFirstKitten(name){
+    kittens.shift(name);
+}
+function destructivelyRemoveLastKitten(name){
+    kittens.pop(name);
+}
+function prependKitten(name){
+    const newKittens=[name, ...kittens];
+return newKittens;
+}
+function removeLastKitten(name){
+const cats= kittens.slice(0,kittens.length-1);
+return cats;
+}
+function removeFirstKitten(name){
+    const cats= kittens.slice(1)
+    return cats
+}
